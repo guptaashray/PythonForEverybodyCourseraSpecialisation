f26138dc8139158a81b955300cb71cf6f8574f76
Ashray Gupta <43576436+guptaashray@users.noreply.github.com>
2019-10-16 21:45:33 +0530

    Create Extracting Data With Regular Expressions.py

---
+++ Using Python to Access Web Data/Week 2/Extracting Data With Regular Expressions.py
@@ -0,0 +1,16 @@
+'''
+In this assignment you will read through and parse a file with text and numbers.
+You will extract all the numbers in the file and compute the sum of the numbers.
+'''
+import re
+fname = input('Enter File name :')
+handle = open(fname)
+sum=0
+count = 0
+for line in handle:
+	f = re.findall('[0-9]+',line)
+	for num in f:		
+		if num >= [0]:			
+			count = count + 1
+			sum = sum + int(num)
+print('There are',count,'values with a sum =',sum)
