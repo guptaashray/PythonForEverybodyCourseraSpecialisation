1240ed725e3e1e9074b9bc6f0d355e0b7d2597ff
Ashray Gupta <43576436+guptaashray@users.noreply.github.com>
2019-10-16 23:02:58 +0530

    Update roster.py

--- Using Databases with Python/Week 4/roster.py
+++ Using Databases with Python/Week 4/roster.py
@@ -9,14 +9,17 @@ cur.executescript('''
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Member;
 DROP TABLE IF EXISTS Course;
+
 CREATE TABLE User (
     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name   TEXT UNIQUE
 );
+
 CREATE TABLE Course (
     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     title  TEXT UNIQUE
 );
+
 CREATE TABLE Member (
     user_id     INTEGER,
     course_id   INTEGER,
@@ -25,8 +28,9 @@ CREATE TABLE Member (
 )
 ''')
 
-fname = raw_input('Enter file name: ')
-if ( len(fname) < 1 ) : fname = 'roster_data.json'
+fname = input('Enter file name: ')
+if len(fname) < 1:
+    fname = 'roster_data.json'
 
 # [
 #   [ "Charley", "si110", 1 ],
@@ -39,23 +43,22 @@ for entry in json_data:
 
     name = entry[0];
     title = entry[1];
-    role = entry[2];
-    print name, title, role
+    instructor = entry[2];
 
-    cur.execute('''INSERT OR IGNORE INTO User (name) 
-        VALUES ( ? )''', ( name, ) )
+    print((name, title, instructor))
+
+    cur.execute('''INSERT OR IGNORE INTO User (name)
+        VALUES ( ? )''', (name, ))
     cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
     user_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Course (title) 
+    cur.execute('''INSERT OR IGNORE INTO Course (title)
         VALUES ( ? )''', ( title, ) )
     cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
     course_id = cur.fetchone()[0]
 
-    
-    
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id, role) VALUES ( ?, ?, ? )''', 
-        ( user_id, course_id, role ) )
+        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
+        (user_id, course_id, instructor))
 
     conn.commit()
